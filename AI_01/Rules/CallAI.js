bce09e6939ce57e192019f489ebc9d4d731c85b1
Jheison Alberto Urzola Hernández <jheison.urzola@sap.com>
2025-04-23 11:44:15 +0000

    AI_01 2

--- AI_01/Rules/CallAI.js
+++ AI_01/Rules/CallAI.js
@@ -3,66 +3,77 @@
  * @param {IClientAPI} clientAPI
  */
 export default function CallAI(clientAPI) {
-    //clientAPI.showActivityIndicator();
-    clientAPI.getPageProxy().getAppClientData().body = {
-        "orchestration_config": {
-            "module_configurations": {
-                "templating_module_config": {
-                    "template": [
-                    {
-                        "role": "system",
-                        "content": "Content"
+    try{
+        //clientAPI.showActivityIndicator();
+        clientAPI.getPageProxy().getAppClientData().body = {
+            "orchestration_config": {
+                "module_configurations": {
+                    "templating_module_config": {
+                        "template": [
+                        {
+                            "role": "system",
+                            "content": "Content"
+                        },
+                        {
+                            "role": "user",
+                            "content": [
+                                {
+                                    "type": "text",
+                                    "text": "Solicitud: {{ ?input }}"
+                                }
+                            ]
+                        }
+                    ]
                     },
-                    {
-                        "role": "user",
-                        "content": [
-                            {
-                                "type": "text",
-                                "text": "Solicitud: {{ ?input }}"
-                            }
-                        ]
+                    "llm_module_config": {
+                        "model_name": "gpt-4o-mini",
+                        "model_params": {
+                            "max_tokens": 16384,
+                            "temperature": 1,
+                            "frequency_penalty": 0.0,
+                            "presence_penalty": 0.0
+                        },
+                        "model_version": "2024-07-18"
                     }
-                ]
-                },
-                "llm_module_config": {
-                    "model_name": "gpt-4o-mini",
-                    "model_params": {
-                        "max_tokens": 16384,
-                        "temperature": 1,
-                        "frequency_penalty": 0.0,
-                        "presence_penalty": 0.0
-                    },
-                    "model_version": "2024-07-18"
                 }
+            },
+            "input_params": {
+                "input": "input"
             }
-        },
-        "input_params": {
-            "input": "input"
-        }
-    };
-    
-    var responseChat = clientAPI.executeAction({
-        "Name": "/AI_01/Actions/Completion.action"
-    });
-    /*
-    let chatResponseText = responseChat?.data?.orchestration_result?.choices?.[0]?.message?.content || "Error";
+        };
+        
+        var responseChat = clientAPI.executeAction({
+            "Name": "/AI_01/Actions/Completion.action"
+        });
+        /*
+        let chatResponseText = responseChat?.data?.orchestration_result?.choices?.[0]?.message?.content || "Error";
 
-    if (chatResponseText == "Error") {
+        if (chatResponseText == "Error") {
+            clientAPI.executeAction({
+                "Name": "/MDK024/Actions/GenericToastMessage.action",
+                "Properties": {
+                    "Message": "Error "
+                }
+            });
+            clientAPI.dismissActivityIndicator();
+            return;
+        }
+
         clientAPI.executeAction({
             "Name": "/MDK024/Actions/GenericToastMessage.action",
             "Properties": {
-                "Message": "Error "
+                "Message": chatResponseText
+            }
+        });
+        */
+    }catch(error){
+        clientAPI.executeAction({
+            "Name": "/AI_01/Actions/GenericToastMessage.action",
+            "Properties": {
+                "Message": "Error "+error
             }
         });
         clientAPI.dismissActivityIndicator();
-        return;
+        return;        
     }
-
-    clientAPI.executeAction({
-        "Name": "/MDK024/Actions/GenericToastMessage.action",
-        "Properties": {
-            "Message": chatResponseText
-        }
-    });
-    */
 }
