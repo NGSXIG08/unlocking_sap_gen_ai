e97f26606c0f125ca686b8828764aa1bedbb2189
Jheison Alberto Urzola Hernández <jheison.urzola@sap.com>
2025-05-17 21:15:44 -0500

    Update OpenAILikeAPICompletion.py

--- AI_05/Generative-AI-Hub-SDK/OpenAILikeAPICompletion.py
+++ AI_05/Generative-AI-Hub-SDK/OpenAILikeAPICompletion.py
@@ -1,8 +1,35 @@
-from gen_ai_hub.proxy.native.openai import completions
-response = completions.create(
-    model_name="gemini-1.5-flash",
-    prompt="The Answer to the Ultimate Question of Life, the Universe, and Everything is",
-    max_tokens=7,
-    temperature=0
+YOUR_API_URL = "https://api.ai.prod.us-east-1.aws.ml.hana.ondemand.com/v2/inference/deployments/d4f47cb57c6f627a"
+
+from gen_ai_hub.orchestration.models.message import SystemMessage, UserMessage
+from gen_ai_hub.orchestration.models.template import Template, TemplateValue
+
+template = Template(
+    messages=[
+        SystemMessage("You are a helpful translation assistant."),
+        UserMessage(
+            "Translate the following text to {{?to_lang}}: {{?text}}"
+        ),
+    ],
+    defaults=[
+        TemplateValue(name="to_lang", value="German"),
+    ],
 )
-print(response)
+
+from gen_ai_hub.orchestration.models.llm import LLM
+
+llm = LLM(name="gpt-4o", version="latest", parameters={"max_tokens": 256, "temperature": 0.2})
+
+from gen_ai_hub.orchestration.models.config import OrchestrationConfig
+
+config = OrchestrationConfig(
+    template=template,
+    llm=llm,
+)
+
+from gen_ai_hub.orchestration.service import OrchestrationService
+
+orchestration_service = OrchestrationService(api_url=YOUR_API_URL, config=config)
+result = orchestration_service.run(template_values=[
+    TemplateValue(name="text", value="The Orchestration Service is working!")
+])
+print(result.orchestration_result.choices[0].message.content)
