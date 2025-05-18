57315c19992153f8d8f6a08ce0153c512b7bcfb3
Jheison Alberto Urzola Hernández <jheison.urzola@sap.com>
2025-05-17 20:55:19 -0500

    Update OpenAILikeAPICompletion.py

--- AI_05/Generative-AI-Hub-SDK/OpenAILikeAPICompletion.py
+++ AI_05/Generative-AI-Hub-SDK/OpenAILikeAPICompletion.py
@@ -1,6 +1,6 @@
 from gen_ai_hub.proxy.native.openai import completions
 response = completions.create(
-    model_name="mistralai--mistral-large-instruct",
+    model_name="gpt-4-32k",
     prompt="The Answer to the Ultimate Question of Life, the Universe, and Everything is",
     max_tokens=7,
     temperature=0
