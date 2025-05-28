489a0b715a0f7d45c33334677786afbc5c06e34d
Jheison Alberto Urzola Hernández <jheison.urzola@sap.com>
2025-05-28 04:07:14 -0500

    Update Step8.py

--- AI_06/Generative-AI-Hub-SDK/Step8.py
+++ AI_06/Generative-AI-Hub-SDK/Step8.py
@@ -221,5 +221,5 @@ f_8 = partial(send_request, prompt=prompt_8, **option_lists)
 
 response = f_8(input=mail["message"])
 
-overall_result["basic--llama3-70b"] = evalulation_full_dataset(test_set_small, f_8)
+overall_result["basic--mixtral-small"] = evalulation_full_dataset(test_set_small, f_8)
 pretty_print_table(overall_result)
